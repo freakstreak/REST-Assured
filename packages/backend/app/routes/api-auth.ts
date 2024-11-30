6d6fadf73f93da292c37bcb4573422b6ca98efeb
Sai <kandimalla@geekyants.com>
2024-11-30 15:29:51 +0530

    auth actions

--- packages/backend/app/routes/api-auth.ts
+++ packages/backend/app/routes/api-auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import AuthController from "../controllers/authController";
+import authenticate from "../middleware/authenticate";
 const router = express.Router();
 
 router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
-router.get("/me", AuthController.me);
+router.get("/me", authenticate, AuthController.me);
 
 export default router;
